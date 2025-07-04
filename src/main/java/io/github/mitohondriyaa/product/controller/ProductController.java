63121c7553cd66056cdf410c38d2cd3aa1ef054e
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-04 22:21:16 +0300

    Add get all products method

--- src/main/java/io/github/mitohondriyaa/product/controller/ProductController.java
+++ src/main/java/io/github/mitohondriyaa/product/controller/ProductController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/product")
@@ -17,5 +19,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+
     }
 }
--- src/main/java/io/github/mitohondriyaa/product/service/ProductService.java
+++ src/main/java/io/github/mitohondriyaa/product/service/ProductService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -26,5 +28,12 @@ public class ProductService {
         log.info("Product created successfully");
 
         return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
     }
 }
