1ffa5a8ee3a535a2efa245cf81234758b5c38e2f
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-04 20:51:02 +0300

    Add product model

---
+++ src/main/java/io/github/mitohondriyaa/product/model/Product.java
@@ -0,0 +1,19 @@
+package io.github.mitohondriyaa.product.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document("product")
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
