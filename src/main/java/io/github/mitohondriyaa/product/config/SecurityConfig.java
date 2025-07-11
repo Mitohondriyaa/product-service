c7d9203334f59d722e3a06deae1d0a1ca5727d1b
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-11 13:49:55 +0300

    Integrate Swagger UI for API documentation

--- src/main/java/io/github/mitohondriyaa/product/config/SecurityConfig.java
+++ src/main/java/io/github/mitohondriyaa/product/config/SecurityConfig.java
@@ -10,9 +10,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorize -> authorize.anyRequest()
+        return http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html")
+                .permitAll()
+                .anyRequest()
                 .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(Customizer
+                    .withDefaults()))
+            .build();
     }
 }
