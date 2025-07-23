05e2cea51004ca8e9c2feebe3d75f9e11a633e54
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-23 19:58:32 +0300

    Rename bean to match expected bean name

--- src/main/java/io/github/mitohondriyaa/product/service/RedisCacheService.java
+++ src/main/java/io/github/mitohondriyaa/product/service/RedisCacheService.java
@@ -9,17 +9,17 @@ import java.time.Duration;
 @Service
 @RequiredArgsConstructor
 public class RedisCacheService {
-    private final RedisTemplate<String, Object> redisCacheTemplate;
+    private final RedisTemplate<String, Object> redisCacheRedisTemplate;
 
     public void setValue(String key, Object value) {
-        redisCacheTemplate.opsForValue().set(key, value, Duration.ofMinutes(10));
+        redisCacheRedisTemplate.opsForValue().set(key, value, Duration.ofMinutes(10));
     }
 
     public Object getValue(String key) {
-        return redisCacheTemplate.opsForValue().get(key);
+        return redisCacheRedisTemplate.opsForValue().get(key);
     }
 
     public void delete(String key) {
-        redisCacheTemplate.delete(key);
+        redisCacheRedisTemplate.delete(key);
     }
 }
