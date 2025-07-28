0d1e146f4f426bba4b2f091a0da233223055e2ec
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-07-28 18:31:32 +0300

    Add authentication and authorization error handling

--- src/main/java/io/github/mitohondriyaa/product/config/SecurityConfig.java
+++ src/main/java/io/github/mitohondriyaa/product/config/SecurityConfig.java
@@ -5,13 +5,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @Profile("!test")
 public class SecurityConfig {
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write(
+                """
+                    {
+                        "code": "UNAUTHORIZED",
+                        "message": "User not authenticated"
+                    }
+                    """
+            );
+        };
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType("application/json");
+            response.getWriter().write("""
+                    {
+                        "code": "FORBIDDEN",
+                        "message": "User not authorized"
+                    }
+                """);
+        };
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -30,6 +63,8 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2
+                .authenticationEntryPoint(authenticationEntryPoint())
+                .accessDeniedHandler(accessDeniedHandler())
                 .jwt(jwt -> jwt.jwtAuthenticationConverter(new KeycloakRealmRoleConverter())))
             .build();
     }
