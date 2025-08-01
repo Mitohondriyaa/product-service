7ddfa3e12d77e3da4624e1aca5f97462be314b10
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-01 13:50:53 +0300

    Add validation exception handle logic

--- src/main/java/io/github/mitohondriyaa/product/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/mitohondriyaa/product/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package io.github.mitohondriyaa.product.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +19,23 @@ public class GlobalExceptionHandler {
         Info info = new Info(exception.getMessage());
 
         return new ResponseEntity<>(info, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<MultipleInfo> handleValidationException(
+        MethodArgumentNotValidException exception
+    ) {
+        MultipleInfo multipleInfo = new MultipleInfo(
+            exception.getBindingResult().getFieldErrors().stream().collect(
+                Collectors.toMap(
+                    FieldError::getField,
+                    DefaultMessageSourceResolvable::getDefaultMessage,
+                    (existing, duplicate) -> existing + ", " +  duplicate
+                )
+            )
+        );
+
+        return new ResponseEntity<>(multipleInfo, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
