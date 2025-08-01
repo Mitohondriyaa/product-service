b88f1b990d4ab08241160b9498a65ea14384a82c
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-01 13:22:39 +0300

    Change Info type to record

--- src/main/java/io/github/mitohondriyaa/product/exception/Info.java
+++ src/main/java/io/github/mitohondriyaa/product/exception/Info.java
@@ -1,14 +1,3 @@
 package io.github.mitohondriyaa.product.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class Info {
-    private String message;
-}
+public record Info(String message) {}
